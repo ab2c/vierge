adbd9d06aa2cecf48d3f0698079f63da6e43c58b
bulle2com <bulle2com@192.168.0.135>
2019-03-20 09:01:43 +0100

    rajout titre

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>titre</title>
     </head>
     <body>
         <?php
